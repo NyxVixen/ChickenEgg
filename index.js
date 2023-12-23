1b2327c855a45820a87f437ee38c8362e1e746a7
Anastasia Anderson <124330987+NyxVixen@users.noreply.github.com>
2023-12-23 13:39:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const chickenBtn = document.getElementById('chicken-btn')
 const eggBtn = document.getElementById('egg-btn')
+const choiceBtns = document.getElementById('choice-btns')
+const heading = document.getElementById('heading')
 
 // document.querySelector('img').src
 
 chickenBtn.addEventListener('click', () => {
-  document.querySelector('img').src = 'https://devsprouthosting.com/images/chicken.jpg'
+  document.querySelector('img').src = 'https://images.unsplash.com/photo-1589050593767-a754dd738587?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNoaWNrZW58ZW58MHx8MHx8fDA%3D'
   document.querySelector('img').alt = "chicken"
-  console.log('You picked chicken!')
+  choiceBtns.style.display = 'none'
+  heading.innerHTML = `You chose Chicken!`
 })
 
 eggBtn.addEventListener('click', () => {
   document.querySelector('img').src = 'https://devsprouthosting.com/images/egg.jpg'
   document.querySelector('img').alt = "egg"
-  console.log('You picked egg!')
+  choiceBtns.style.display = 'none'
+  heading.innerHTML = `You chose Egg!`
 })
