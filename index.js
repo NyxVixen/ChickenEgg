959b54e21fff4439822621eaa3af98ba19b29b5f
Anastasia Anderson <124330987+NyxVixen@users.noreply.github.com>
2023-12-23 13:47:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ chickenBtn.addEventListener('click', () => {
   document.querySelector('img').src = 'https://images.unsplash.com/photo-1589050593767-a754dd738587?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNoaWNrZW58ZW58MHx8MHx8fDA%3D'
   document.querySelector('img').alt = "chicken"
   choiceBtns.style.display = 'none'
-  heading.innerHTML = `You chose Chicken!`
+  heading.innerHTML = `You chose chicken!`
 })
 
 eggBtn.addEventListener('click', () => {
   document.querySelector('img').src = 'https://devsprouthosting.com/images/egg.jpg'
   document.querySelector('img').alt = "egg"
   choiceBtns.style.display = 'none'
-  heading.innerHTML = `You chose Egg!`
+  heading.innerHTML = `You chose egg!`
 })
